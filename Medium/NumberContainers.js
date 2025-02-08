0d13523a42df73163bef2dc04d4556fdbb675f44
yasiramus <yasirayusif@gmail.com>
2025-02-08 22:33:31 +0000

    Using Min Heap with Lazy Update approach

--- Medium/NumberContainers.js
+++ Medium/NumberContainers.js
@@ -29,3 +29,7 @@
 // nc.find(10); // Number 10 is at the indices 1, 2, 3, and 5. Since the smallest index that is filled with 10 is 1, we return 1.
 // nc.change(1, 20); // Your container at index 1 will be filled with number 20. Note that index 1 was filled with 10 and then replaced with 20. 
 // nc.find(10); // Number 10 is at the indices 2, 3, and 5. The smallest index that is filled with 10 is 2. Therefore, we return 2.
+
+// Approach: Using Min Heap with Lazy Update
+// Intuition
+// using min heaps (priority queues) for managing the indices associated with each number. storing them in a priority queue (min heap) to handle the ordering for us in this approach.
